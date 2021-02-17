9735720628c67207f9828c4a2540dc40295f72fd
Stefan Garcés <stefangarces@gmail.com>
2021-02-17 15:30:27 +0000

    Added JS code, but had problem to GET objects from the API.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // API-key: 7899101f669946d4bc57d2e21de4429e
+
+document.addEventListener("DOMContentLoaded", function() {
+  NewsAPI();
+});
+
+async function NewsAPI() {
+  const url = 'HTTPS://newsapi.org/v2/everything?' +
+          'q=Games&' +
+          'from=2021-02-17&' +
+          'sortBy=popularity&' +
+          'apiKey=7899101f669946d4bc57d2e21de4429e';
+
+const req = new Request(url);
+
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
+}
