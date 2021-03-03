ea9f275087747df7f7040f1a7bc948f3147280a6
Stefan Garces <stefangarces@gmail.com>
2021-03-03 21:02:19 +0100

    Added a fetch button to the "doggy information" box.

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", getDogInfo());
 
-let dropMenu = document.querySelector(".dog-selector");
-let dogBreed = dropMenu.text;
-
-console.log(dogBreed);
 
 async function getDogInfo() {
     fetch(`https://api.thedogapi.com/v1/images/search`).then(function(resp) {
@@ -11,6 +7,7 @@ async function getDogInfo() {
     return resp.json()
     }).then(function(data) {
         printDogInfo(data);
+        console.log(data);
 })}
 
 function printDogInfo(data) {
@@ -32,6 +29,7 @@ function printDogInfo(data) {
     dogHeight.innerHTML = "Avarage height: " + dogHeightInfo;
     dogLifespan.innerHTML = "Avarage Lifespan: " + dogLifespanInfo;
 }
+
 // breed: [0].breeds[0].bred_for
 // breed group: [0].breeds[0].breed_group
 // breed name: [0].breeds[0].name
